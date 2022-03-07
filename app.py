7ba830296d7d3c7af4f34c73420a2acab9d8a384
Mikhail Doroshenko <mixdorosh@gmail.com>
2022-03-07 15:53:18 +0700

    add pages

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def index():
     return 'Hello'
+
 
 @app.route('/about')
 def about():
     return 'ABout page'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page: ' + name + ' - ' + str(id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
