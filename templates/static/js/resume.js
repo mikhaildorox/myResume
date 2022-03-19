d2e1eda7c0daa2a2ebd5a6bd918899a5a6975464
Mikhail Doroshenko <mixdorosh@gmail.com>
2022-03-19 17:22:14 +0700

    v1.0

--- templates/static/js/resume.js
+++ templates/static/js/resume.js
@@ -1,14 +1,13 @@
-document.onclick = function(e){
-  if(e.target.tagName == 'A'){
-      if(e.target.id == 'a1'){
-          document.querySelector('#output').textContent = document.querySelector('#text').textContent + ' 150 символов';
-      }
-      if(e.target.id == 'a2'){
-          document.querySelector('#output').textContent = document.querySelector('#text').textContent + ' 120 символов';
-      }
-      if(e.target.id == 'a3'){
-          document.querySelector('#output').textContent = document.querySelector('#text').textContent + ' 200 символов';
-      }
-  }else
-      document.querySelector('#output').textContent = 'Тут будет выводится текст из ссылок';
-}
+var about = 'Few words about me';
+var skills = 'Python, html, css';
+var education = 'Bachelar DC';
+var game = `playt fugit molestias,jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjexpeditis officiis quos cum.`;
+
+$(function() {
+    var insert = $('#insert');
+    $('a[rel="insert"]').click(function() {
+        var a = $(this).attr('href');
+        insert.css('display', 'none').html(window[a]).fadeIn(300);
+        return false;
+    });
+}); 
