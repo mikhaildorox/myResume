b3ae21f64d057dedabf46149be921b47aa1505f0
Mikhail Doroshenko <mixdorosh@gmail.com>
2022-05-03 00:26:25 +0700

    css, js, html. fonts

--- templates/static/js/resume.js
+++ templates/static/js/resume.js
@@ -1,13 +1,51 @@
 var about = 'Few words about me';
 var skills = 'Python, html, css';
 var education = 'Bachelar DC';
-var game = `playt fugit molestias,jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjexpeditis officiis quos cum.`;
+var game = `quos cum.`;
 
-$(function() {
-    var insert = $('#insert');
-    $('a[rel="insert"]').click(function() {
+// set up text to print, each item in array is new line
+var aText = new Array(game);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("insert");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+        }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+$(function () {
+    var insert = $('#insert');  
+    $('a[rel="insert"]').click(function () {
         var a = $(this).attr('href');
         insert.css('display', 'none').html(window[a]).fadeIn(300);
         return false;
     });
-}); 
+
+    // if (game_link.classList.contains("for_game")) {
+    //     document.getElementById('insert').classList.remove('for_game');
+    // } else {
+    //     document.getElementById('insert').classList.add('for_game');
+    // }
+});
